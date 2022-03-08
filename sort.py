0e90109697691c72b677dd45af780e1c8243e133
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-03-07 22:07:05 -0800

    Question 1 take 1

--- sort.py
+++ sort.py
@@ -1,1 +1,10 @@
-print('Hellos')
+def sort_list(myList):
+...     n = len(mylist)
+...     for i in range(n):
+...             for j in range(n-i-1):
+...                     if myList[j] > myList[j+1]:
+...                             temp = myList[j]
+...                             myList[j] = myList[j+1]
+...                             myList[j+1] = temp
+...     return myList
+
