e314970a57cb79acb9770cdd2a63547aa650a988
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-03-08 17:42:05 -0800

    Question 1 take 3

--- sort.py
+++ sort.py
@@ -1,10 +1,10 @@
 def sort_list(myList):
-     n = len(mylist)
+     n = len(myList)
      for i in range(n):
              for j in range(n-i-1):
                      if myList[j] > myList[j+1]:
                              temp = myList[j]
                              myList[j] = myList[j+1]
                              myList[j+1] = temp
-     return myList
+     print(myList)
 
