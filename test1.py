12518976ea2c9b3ad3215f939f07b355ac412f27
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-03-10 12:05:27 -0800

    Question2 take1

---
+++ test1.py
@@ -0,0 +1,18 @@
+my_file = open("document.txt", "r")
+data = my_file.read()
+data_into_list = data.split(" ")
+from collections import Counter
+x = Counter(data_into_list)
+word_counter = {}
+for word in data_into_list:
+     if word in word_counter:
+         word_counter[word] += 1
+     else:
+         word_counter[word] = 1
+popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
+print(f'{popular_words[0]}: { x[popular_words[0]]}')
+print(f'{popular_words[1]}: { x[popular_words[1]]}')
+print(f'{popular_words[2]}: { x[popular_words[2]]}')
+print(f'{popular_words[3]}: { x[popular_words[3]]}')
+print(f'{popular_words[4]}: { x[popular_words[4]]}')
+
