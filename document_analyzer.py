12e8b281dec6457d9248ea991f8b9e23e7c03eb5
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-03-10 19:28:11 -0800

    Question 2 take 8

--- document_analyzer.py
+++ document_analyzer.py
@@ -3,6 +3,7 @@ data = my_file.read()
 data_into_list = data.split(" ")
 from collections import Counter
 x = Counter(data_into_list)
+data_into_list.sort()
 word_counter = {}
 for word in data_into_list:
      if word in word_counter:
@@ -10,7 +11,7 @@ for word in data_into_list:
      else:
          word_counter[word] = 1
 popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
-print(f'\r{popular_words[0]}: { x[popular_words[0]]}')
+print(f'\n{popular_words[0]}: { x[popular_words[0]]}')
 print(f'{popular_words[1]}: { x[popular_words[1]]}')
 print(f'{popular_words[2]}: { x[popular_words[2]]}')
 print(f'{popular_words[3]}: { x[popular_words[3]]}')
