ac1eb97b393bc59b6e12a5eab44a844bba732fc8
Mohammad-Rahhal <57151103+Mohammad-Rahhal@users.noreply.github.com>
2022-03-10 21:16:09 -0800

    Question 5 take 2

--- tracker.py
+++ tracker.py
@@ -1,9 +1,6 @@
 def func_counter(func):
-        
-        def wrapper(x):
-            
-            func(x)
+        def wrapper(*x):
+            func(*x)
             wrapper.counter = wrapper.counter + 1
-            
         wrapper.counter = 0 
         return wrapper
